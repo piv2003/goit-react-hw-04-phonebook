ee5912d825098201e8e633cf8ca5fcdba516ccda
Ihor Poliakov <piv2003@gmail.com>
2023-07-10 23:54:43 +0200

    Update Form.js

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,5 +1,5 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Component } from 'react';
 import { FormBox, InputName, InputTel, Button } from './Form.styled';
 class Form extends Component {
   state = {
